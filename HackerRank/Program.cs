fe1b36d600bfd53caf0d1ef71dc89484c944ccee
thedakotes <lantripdakota@gmail.com>
2025-05-05 18:39:21 -0400

    Move swagger out of dev only

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -27,18 +27,13 @@ builder.Services.AddMemoryCache();
 builder.Services.AddScoped<IHackerRankService, HackerRankService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors(MyAllowSpecificOrigins);
 
